4591000a824c76b466df65a534cd2590f66f3b98
numnum-lab <numchakhatanon@gmail.com>
2025-09-23 20:38:36 +0700

    Update app.py

--- 1_model/1_model/deploy/app.py
+++ 1_model/1_model/deploy/app.py
@@ -105,7 +105,58 @@ if "yolo_model" not in st.session_state:
 
 #Infomation section 
     st.sidebar.title("trast")
+#Infomation section 
+st.sidebar.title("Student Information")
+name_container = st.sidebar.empty()
+id_container = st.sidebar.empty()
+name_container.info('Name: Unknown')
+id_container.success('ID: Unknown')
+if choice == "Picture":
+    st.title("Face Recognition App")
+    st.write(PICTURE_PROMPT)
+    uploaded_images = st.file_uploader("Upload",type=['jpg','png','jpeg'],accept_multiple_files=True)
+    if len(uploaded_images) != 0:
+        #Read uploaded image with face_recognition
+        for image in uploaded_images:
+            image = frg.load_image_file(image)
+            image, name, id = recognize(image,TOLERANCE) 
+            name_container.info(f"Name: {name}")
+            id_container.success(f"ID: {id}")
+            st.image(image)
+    else: 
+        st.info("Please upload an image")
+    
+elif choice == "Webcam":
+    st.title("Face Recognition App")
+    st.write(WEBCAM_PROMPT)
+    #Camera Settings
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    FRAME_WINDOW = st.image([])
+    
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            st.error("Failed to capture frame from camera")
+            st.info("Please turn off the other app that is using the camera and restart app")
+            st.stop()
+        image, name, id = recognize(frame,TOLERANCE)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #Display name and ID of the person
+        
+        name_container.info(f"Name: {name}")
+        id_container.success(f"ID: {id}")
+        FRAME_WINDOW.image(image)
 
+with st.sidebar.form(key='my_form'):
+    st.title("Developer Section")
+    submit_button = st.form_submit_button(label='REBUILD DATASET')
+    if submit_button:
+        with st.spinner("Rebuilding dataset..."):
+            build_dataset()
+        st.success("Dataset has been reset")
+        
     # Disposal Guide
     st.markdown("---")
     st.subheader("📋 Disposal Guide")
